f9e2268d5839bb71a8198c84823cd8453dff0552
gameoflord012 <manhhieu012@gmail.com>
2021-07-19 11:46:57 +0700

    EditorGUI and EditorGUILayout

--- RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        [MenuItem("Window/Dialogue Editor")]
+        static public void ShowEditorWindow()
+        {
+            GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
 
+        [OnOpenAssetAttribute(1)]
+        static public bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
+        }        
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("Hello World 1");
+            EditorGUILayout.LabelField("Hello World 2");
+            EditorGUILayout.LabelField("Hello World 3");
+        }
     }
 }
